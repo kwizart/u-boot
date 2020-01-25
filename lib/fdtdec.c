2522ee3c5d85eaec2f34dbfbcf4e3c1031bdf9af
Marc Dietrich <marvin24@gmx.de>
2020-01-25 18:35:55 +0100

    ARM: tegra: add nvec driver
    Adopted version of nvec driver from linux kernel with
    keyboard support only.
    
    Signed-off-by: Andrey Danin <danindrey@mail.ru>
    [squashed, forward ported, and cleaned up]
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- lib/fdtdec.c
+++ lib/fdtdec.c
@@ -45,6 +45,7 @@ static const char * const compat_names[COMPAT_COUNT] = {
 	COMPAT(NVIDIA_TEGRA20_EMC, "nvidia,tegra20-emc"),
 	COMPAT(NVIDIA_TEGRA20_EMC_TABLE, "nvidia,tegra20-emc-table"),
 	COMPAT(NVIDIA_TEGRA20_NAND, "nvidia,tegra20-nand"),
+	COMPAT(NVIDIA_TEGRA20_NVEC, "nvidia,tegra20-nvec"),
 	COMPAT(NVIDIA_TEGRA124_XUSB_PADCTL, "nvidia,tegra124-xusb-padctl"),
 	COMPAT(NVIDIA_TEGRA210_XUSB_PADCTL, "nvidia,tegra210-xusb-padctl"),
 	COMPAT(SAMSUNG_EXYNOS_USB_PHY, "samsung,exynos-usb-phy"),
